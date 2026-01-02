6e22e5110458f972bb0fb4663095aec5e0110901
Nit <nitithon.taksaphan@gmail.com>
2026-01-02 23:09:03 +0700

    Refactor: Simplify App component by removing commented-out LogInPage and SignUpPage imports, enhancing clarity and focus on active components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import "./App.css";
 import { NavBar, Footer } from "./components/layout";
 import LandingPage from "./components/landing-page/LandingPage";
-import LogInPage from "./components/AuthPage/LogInPage";
-import SignUpPage from "./components/AuthPage/SignUpPage";
 
 function App() {
   return (
     <>
-      {/* <NavBar />
+      <NavBar />
       <LandingPage />
-      <Footer /> */}
-      <LogInPage />
-      {/* <SignUpPage /> */}
+      <Footer /> */
     </>
   );
 }
