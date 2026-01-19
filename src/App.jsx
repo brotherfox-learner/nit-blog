323cfc4b3f739f89476a79673baeb8f33fbd8278
Nit <nitithon.taksaphan@gmail.com>
2026-01-20 02:47:24 +0700

    feat: Add MemberPage component and update routing in App.jsx to include member page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import LandingPage from "./pages/LandingPage";
 import ArticlePage from "./pages/ArticlePage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
+import MemberPage from "./components/member-page";
 import { Routes, Route } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import AdminPage from "./pages/AdminPage";
+import MemberPage from "./pages/MemberPage";
 
 function App() {
   return (
     <div className="w-full max-w-full overflow-x-hidden">
-
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/article" element={<ArticlePage />} />
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login" element={<LogInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/member" element={<MemberPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
